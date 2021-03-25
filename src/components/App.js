7117507371b9f1ce5a1fa9526e0b1288f80d137a
MariamZiade <mariam.ziade@gmail.com>
2021-03-25 21:54:26 +0200

    Added components

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+    return (
+      <div>
+        <h1 style={{ color: "red" }}>To Do List</h1>
+  
+        <p>Write your to do list for tomorrow</p>
+        <ul>
+          <li>Wake up at 5:00 a.m.</li>
+          <li>Ride my Bike</li>
+          <li>Watch Coursera</li>
+        </ul>
+      </div>
+    );
+  }
+  export default App;
